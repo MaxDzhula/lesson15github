61f11705285746d1a6ad99a3187d86a98bca8fa7
p45 <p45@edu.itstep.lv>
2025-06-14 14:26:53 +0300

    New function commit

--- lesson15github/lesson15github.cpp
+++ lesson15github/lesson15github.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
+
+
+void funcHello() {
+    cout << "Hello Github";
+}
+
 void main()
 {
     cout << "We commit this code with VS Studio!!!";
+    funcHello();
 }
 
